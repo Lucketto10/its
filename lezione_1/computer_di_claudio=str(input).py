42ab7f981ef6ab23e1e882b8a2491f1591e33837
Lucketto10 <luca5.taggiasco@gmail.com>
2025-02-19 11:27:47 +0100

    chao

---
+++ lezione_1/computer_di_claudio=str(input).py
@@ -0,0 +1,7 @@
+computer_di_claudio=str(input("inserisci computer claudio "))
+
+if computer_di_claudio == "e' stato hackerato":
+    print (f"godooooooooooooooooo")
+else:
+    print (f" oh cabbo")
+ 
